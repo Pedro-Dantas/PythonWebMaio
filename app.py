ada772ebff7608d0a50d9357b41316cd51208dd3
Pedro Dantas <dev.pedrodantas@gmail.com>
2024-05-15 21:45:05 +0000

    added requirements and gitignore

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask("Olá")
 
 @app.route('/')
-
 def ola():
-    user_agent = request.headers.get('User-Agent')
-    print("User-Agent:", user_agent)
-    return "Olá Mundo " + user_agent
+    return "Olá Mundo"
